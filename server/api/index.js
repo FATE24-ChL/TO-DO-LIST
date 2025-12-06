c8ad31e83cb1029758ba7c1576fbffdccb43625d
FATE24-ChL <chrislelouis2006@gmail.com>
2025-12-06 19:24:37 +0530

    this is my first commit

---
+++ server/api/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import auth from './routes/auth.js'
+
+const app = express()
+const port = 3000
+app.use(express.json());
+
+
+app.use('/api/auth/', auth)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ server/api/routes/auth.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const route = express.Router();
+
+route.post('/login',(req, res)=>{
+    const request =  req.body;
+    if(!request)
+    {
+        return res.status(400).json({error: 'No data provided'});
+    }
+    else{
+        if(request.username === 'pravin' && request.password === 'pravinshinde')
+        {
+            return res.json({message: 'login successful'});
+        }
+        else{
+            return res.status(401).json({error: 'Invalid credentials'});
+        }
+    }
+})
+
+route.get('/create-user',(req, res)=>{
+    res.send('user created');
+})
+
+export default route;
