c5a0afd6d58bdd8c680fc3880b521640b22bf285
FATE24-ChL <chrislelouis2006@gmail.com>
2025-12-10 21:40:54 +0530

    dynamic list added

--- client/todoclient/app/page.tsx
+++ client/todoclient/app/page.tsx
@@ -1,20 +1,25 @@
 import link from "next/link";
 
 export default function Home() {
+  const worklist = ["Homework", "Food", "Sleep"];
+
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
       <div className="bg-white w-full max-w-sm p-6 rounded-xl shadow-md">
-        <h2 className="text-lg font-semibold mb-3">Recent Searches</h2>
+        <h2 className="text-lg font-semibold mb-3 text-center">Task Lists</h2>
         <div className="space-y-1 mb-4 text-sm text-gray-700">
-          <p className="border-b pb-1">Search 1</p>
-          <p className="border-b pb-1">Search 2</p>
-          <p className="border-b pb-1">Search 3</p>
+          {
+            worklist.map((e)=>(
+                <p>{e}</p>
+            ))
+          }
         </div>
         <input type="text" placeholder="Enter something..." className="w-full border px-3 py-2 rounded-lg mb-4"/>
         <div className="flex gap-3">
           <button className="flex-1 bg-indigo-600 text-white py-2 rounded-lg">Add</button>
           <button className="flex-1 bg-red-500 text-white py-2 rounded-lg">Delete</button>
         </div>
+
 
       </div>
     </main>
