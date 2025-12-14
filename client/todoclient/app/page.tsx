451debed5816b52b1bf31c918230fa28e8d54bc8
dhruvpoojari1527 <dhruvpoojari1527@gmail.com>
2025-12-14 16:18:38 +0530

    LIST ADDED

--- client/todoclient/app/page.tsx
+++ client/todoclient/app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
 import link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
-  const worklist = ["Homework", "Food", "Sleep"];
+  // const worklist = ["Homework", "Food", "Sleep"];
+
+  const [worklist, setworklist] = useState<string[]>([]);
 
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
@@ -9,19 +13,24 @@ export default function Home() {
         <h2 className="text-lg font-semibold mb-3 text-center">Task Lists</h2>
         <div className="space-y-1 mb-4 text-sm text-gray-700">
           {
-            worklist.map((e)=>(
-                <p>{e}</p>
+            worklist.map((e) => (
+              <p>{e}</p>
             ))
           }
         </div>
-        <input type="text" placeholder="Enter something..." className="w-full border px-3 py-2 rounded-lg mb-4"/>
+        <input id="taskInput" type="text" placeholder="Enter something..." className="w-full border px-3 py-2 rounded-lg mb-4 text-black" />
         <div className="flex gap-3">
-          <button className="flex-1 bg-indigo-600 text-white py-2 rounded-lg">Add</button>
+          <button className="flex-1 bg-indigo-600 text-white py-2 rounded-lg"
+
+            onClick={() => {
+              const taskInput = document.getElementById("taskInput") as HTMLInputElement;
+              setworklist([...worklist, taskInput.value]);
+            }}>Add</button>
           <button className="flex-1 bg-red-500 text-white py-2 rounded-lg">Delete</button>
         </div>
 
 
       </div>
     </main>
-    );
+  );
 }
