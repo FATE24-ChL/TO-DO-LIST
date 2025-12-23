499ec8733d27d70dbf338fbbbc33b2aa824ec987
dhruvpoojari1527 <dhruvpoojari1527@gmail.com>
2025-12-23 21:27:49 +0530

    nice

--- client/todoclient/app/page.tsx
+++ client/todoclient/app/page.tsx
@@ -3,18 +3,27 @@ import link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+
   // const worklist = ["Homework", "Food", "Sleep"];
 
   const [worklist, setworklist] = useState<string[]>([]);
   const [ActiveTask, setActiveTask] = useState<string>("");
+  // const [templist, settemplist] = useState<string[]>([]);
 
   const handeDelete = () => {
     console.log(ActiveTask);
     const templist = worklist;
     templist.splice(templist.indexOf(ActiveTask), 1);
-    setworklist(templist);
-    console.log(worklist);
+    console.log(templist);
+    // setworklist(templist);
+    setworklist([...templist]);
   }
+
+  const addTask = () => {
+    const taskInput = document.getElementById("taskInput") as HTMLInputElement;
+    setworklist([...worklist, taskInput.value]);
+    taskInput.value = "";
+  };
 
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
@@ -34,8 +43,7 @@ export default function Home() {
           <button className="flex-1 bg-indigo-600 text-white py-2 rounded-lg"
 
             onClick={() => {
-              const taskInput = document.getElementById("taskInput") as HTMLInputElement;
-              setworklist([...worklist, taskInput.value]);
+              addTask();
             }}>Add</button>
           <button
             onClick={() => {
